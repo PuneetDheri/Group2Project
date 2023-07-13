ded039b5b09136f399ead5d4a1aef4288da0f2eb
PuneetDheri <puneet.dheri@gmail.com>
2023-07-12 21:39:49 -0400

    created the MediaItem class and set up properties (Puneet)

---
+++ MyMediaLibrary/MediaItem.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace MyMediaLibrary
+{
+    // enums for the status of media item in library database
+    public enum MediaStatus
+    {
+        Watching,
+        Completed,
+        OnHold,
+        PlanToWatch,
+        Dropped
+    }
+
+    // enums for the genre of a media item in library database
+    public enum MediaGenre
+    {
+        Action,
+        Adventure,
+        Comedy,
+        Drama,
+        Fantasy,
+        Horror,
+        Mystery,
+        Romance,
+        SciFi,
+        Thriller
+    }
+
+
+    public class MediaItem
+	{
+        //used protected to allow subclasses to modify the properties
+        protected string Title { get; set; }
+        protected TimeSpan Duration { get; set; }
+        protected DateTime ReleaseDate { get; set; }
+        protected MediaGenre Genre { get; set; }
+        protected MediaStatus Status { get; set; }
+
+        // constructor 
+        public MediaItem(string title, TimeSpan duration, DateTime releaseDate, MediaGenre genre, MediaStatus status)
+        {
+            Title = title;
+            Duration = duration;
+            ReleaseDate = releaseDate;
+            Genre = genre;
+            Status = status;
+        }
+    }
+}
+
