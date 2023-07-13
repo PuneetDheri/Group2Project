1c468d75ea3ef2076b320d511814c21c6f6f71f5
BilalSamoon <samoonb@shernet.sheridancollege.ca>
2023-07-13 01:00:21 -0400

    TV Series Class Added

---
+++ MyMediaLibrary/TVSeries.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace MyMediaLibrary
+{
+    public class TVSeries : MediaItem
+    {
+        public string Creator { get; set; }
+        public int Episodes { get; set; }
+        public int Seasons { get; set; }
+
+        public TVSeries(string title, TimeSpan duration, DateTime releaseDate, MediaGenre genre, MediaStatus status, string creator, int episodes, int seasons)
+            : base(title, duration, releaseDate, genre, status)
+        {
+            Creator = creator;
+            Episodes = episodes;
+            Seasons = seasons;
+        }
+    }
+}
+
