1324b945d00e21e5741f77b5b6d6baec945082bc
terrynillo <nilloj@shernet.sheridancollege.ca>
2023-07-12 20:38:09 -0400

    finished with the user class (until more changes need to be added)

--- MyMediaLibrary/User.cs
+++ MyMediaLibrary/User.cs
@@ -3,9 +3,28 @@ namespace MyMediaLibrary
 {
 	public class User
 	{
-		public User()
+		//both user and email are going to be private values
+		private string _name;
+		private string _email;
+
+		//constructorr
+		public User(string name, string email)
 		{
+			_name = name;
+			_email = email;
 		}
+
+		//methods GetName() and GetEmail() are going to return
+		//the name and the email
+		public string GetName() {
+			return _name;
+		}
+
+		public string GetEmail() {
+
+			return _email;
+		}
+
 	}
 }
 
