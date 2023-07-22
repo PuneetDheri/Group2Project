b15bbbcee88140eaa8f0fe7db1f6151597f1ea79
PuneetDheri <puneet.dheri@gmail.com>
2023-07-22 00:21:53 -0400

    addMedia method created

--- MyMediaLibrary/MediaLibrary.cs
+++ MyMediaLibrary/MediaLibrary.cs
@@ -3,9 +3,22 @@ namespace MyMediaLibrary
 {
 	public class MediaLibrary
 	{
-		public MediaLibrary()
+		private List<MediaItem> mediaItems; //stores list of Media Items
+		public MediaLibrary() //constructer called when  new instace of MediaLibrary is created
 		{
+			mediaItems = new List<MediaItem>();
 		}
-	}
+
+        public void AddMedia(string title, TimeSpan duration, DateTime releaseDate, MediaGenre genre, MediaStatus status)
+        {
+
+            //create a new MediaItem object using the parameters
+            MediaItem mediaItem = new MediaItem(title, duration, releaseDate, genre, status);
+
+
+            //add the mediaItem to the mediaItems list
+            mediaItems.Add(mediaItem);
+        }
+    }
 }
 
