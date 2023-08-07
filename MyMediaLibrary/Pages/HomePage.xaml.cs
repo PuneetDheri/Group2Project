85da8dcff9fd357de968cb8e33d18519e64d6925
terrynillo <nilloj@shernet.sheridancollege.ca>
2023-08-07 12:54:29 -0400

    established the list binding into the homepage with the media library class

--- MyMediaLibrary/Pages/HomePage.xaml.cs
+++ MyMediaLibrary/Pages/HomePage.xaml.cs
@@ -2,6 +2,22 @@
 
 public partial class HomePage : ContentPage
 {
+	//establishing data
+	MediaLibrary _mediaLibrary;
+
+
+	//onproperty change
+	public MediaLibrary MediaLibrary
+	{
+		get { return _mediaLibrary; }
+		set
+		{
+			if (_mediaLibrary == value) { return; }
+			_mediaLibrary = value;
+			OnPropertyChanged();
+		}
+	}
+
 
     string _name;
 
