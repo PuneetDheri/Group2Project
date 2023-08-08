7616592b27638b881e895b0c78ccb6c77e6262bd
PuneetDheri <puneet.dheri@gmail.com>
2023-08-08 02:53:21 -0400

    add button/page implementation (in peogress)

--- MyMediaLibrary/Pages/AddPage.xaml.cs
+++ MyMediaLibrary/Pages/AddPage.xaml.cs
@@ -2,8 +2,28 @@
 
 public partial class AddPage : ContentPage
 {
-	public AddPage()
+    private MediaLibrary _mediaLibrary;
+
+    public AddPage(MediaLibrary mediaLibrary)
 	{
 		InitializeComponent();
-	}
+
+
+        _mediaLibrary = mediaLibrary;
+    }
+
+    
+
+    void OnAddMediaClicked(System.Object sender, System.EventArgs e)
+    {
+        string title = TitleEntry.Text;
+        TimeSpan duration = DurationPicker.Time;
+        MediaGenre genre = (MediaGenre)Enum.Parse(typeof(MediaGenre), GenrePicker.SelectedItem.ToString());
+        MediaStatus status = (MediaStatus)Enum.Parse(typeof(MediaStatus), StatusPicker.SelectedItem.ToString());
+
+        _mediaLibrary.AddMedia(title, duration, DateTime.Now, genre, status);
+
+        Navigation.PopAsync(); // go back (HomePage)
+    }
+
 }
--- MyMediaLibrary/Pages/HomePage.xaml.cs
+++ MyMediaLibrary/Pages/HomePage.xaml.cs
@@ -18,10 +18,19 @@ public partial class HomePage : ContentPage
 
         UserLabel.Text = $"Welcome! {_name}";
 
-		_mediaLibrary.AddMedia("Test", TimeSpan.FromHours(1),  DateTime.Now, MediaGenre.Drama, MediaStatus.PlanToWatch);
+		_mediaLibrary.AddMedia("Test1", TimeSpan.FromHours(1),  DateTime.Now, MediaGenre.Drama, MediaStatus.PlanToWatch);
+        _mediaLibrary.AddMedia("Test2", TimeSpan.FromHours(1), DateTime.Now, MediaGenre.Drama, MediaStatus.PlanToWatch);
+        _mediaLibrary.AddMedia("Test3", TimeSpan.FromHours(1), DateTime.Now, MediaGenre.Drama, MediaStatus.PlanToWatch);
+        _mediaLibrary.AddMedia("Test4", TimeSpan.FromHours(1), DateTime.Now, MediaGenre.Drama, MediaStatus.PlanToWatch);
+
 
         BindingContext = this;
 
     }
 
+    async void OnAddMediaButtonClicked(System.Object sender, System.EventArgs e)
+    {
+        AddPage addPage = new AddPage(_mediaLibrary);
+        await Navigation.PushAsync(addPage);
+    }
 }
