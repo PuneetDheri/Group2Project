0995b32fc31ec8c0d655d31ec6bacafc431f34ce
PuneetDheri <puneet.dheri@gmail.com>
2023-08-11 21:05:33 -0400

    created data access class

---
+++ MyMediaLibrary/DataAccess/MediaLibraryCSV.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Xamarin.KotlinX.Coroutines.Channels;
+
+namespace MyMediaLibrary.DataAccess
+{
+	public class MediaLibraryCSV
+	{
+
+        string _fileName;
+        private MediaLibrary _mediaLibrary;
+
+        public MediaLibraryCSV(string fileName)
+        {
+            _fileName = fileName;
+        }
+
+
+        public void WriteProducts(ObservableCollection<MediaItem> MediaItems)
+        {
+            
+          
+        }
+
+
+
+    }
+}
+
