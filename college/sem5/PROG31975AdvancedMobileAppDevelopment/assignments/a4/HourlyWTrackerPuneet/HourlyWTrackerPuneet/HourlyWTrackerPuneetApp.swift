c7fe4580563c835e798b94605b3997b30e419f6f
PuneetDheri <puneet.dheri@gmail.com>
2024-11-28 23:38:15 -0500

    Initial commit

---
+++ college/sem5/PROG31975AdvancedMobileAppDevelopment/assignments/a4/HourlyWTrackerPuneet/HourlyWTrackerPuneet/HourlyWTrackerPuneetApp.swift
@@ -0,0 +1,17 @@
+//
+//  HourlyWTrackerPuneetApp.swift
+//  HourlyWTrackerPuneet
+//
+//  Created by Puneet Dheri on 2024-11-28.
+//
+
+import SwiftUI
+
+@main
+struct HourlyWTrackerPuneetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
